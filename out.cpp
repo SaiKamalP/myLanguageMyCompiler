eb84ab35057ba9cb542b383e566ad7d5defcb40a
PAIDEMARRY SAI KAMAL <psaikamal12052005@gmail.com>
2023-06-02 00:44:21 +0530

    modified the action of ! to have no space after in code

--- out.cpp
+++ out.cpp
@@ -2,8 +2,22 @@
 
 using namespace std;
 
-int main (){
-	for (long long i  = 0  ; i < 10 ; i++ ){
-		cout<< i *i << endl ;
+void dfs (long long child , long long parent , vector < long long > *adj ){
+	visit (child );
+	for (long long i  = 0  ; i < adj [child ]. size (); i++ ){
+		if (adj [child ][i ]!=parent ){
+			dfs (adj [child ][i ], parent , adj );
+		}
 	}
+}
+int main (){
+	int t ;
+	cin>> t ;
+	for (long long r  = 0  ; r < t ; r++ ){
+		int u , v ;
+		cin>> u >> v ;
+		otherCode ();
+	}
+	int ans =10 ;
+	cout<< ans << " " << ans *ans << endl ;
 }
