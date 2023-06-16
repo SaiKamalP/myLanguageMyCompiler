35edd139f67ff1bc23411e6bbbe892927b200530
PAIDEMARRY SAI KAMAL <psaikamal12052005@gmail.com>
2023-06-17 00:09:15 +0530

    Started project new
    in mylangcompiler.cpp added functions:
    	tokenize(string code)
    	it would take code and split into individual tokens
    
    	To demonstrate this in main function an exampleCode is kept that is printed as tokens.

--- mylangcompiler.cpp
+++ mylangcompiler.cpp
@@ -1,226 +1,130 @@
 #include <bits/stdc++.h>
  
 using namespace std;
-unordered_map<char,int> chartype;
-unordered_map<string,string> wordRephraser;
-int isEmpty(string &s){
-    for(char x:s){
-        if(chartype.count(x) && chartype[x]!=0){
-            return false;
+enum CharTypes{
+    Unknown,
+    TextOrNumber,
+    SpaceOrEndline,
+    SpecialChar
+};
+unordered_map<char,CharTypes> CHAR_TYPE;
+bool isCharEmpty(char c){
+    return !(CHAR_TYPE[c]==TextOrNumber || CHAR_TYPE[c]==SpecialChar);
+}
+int isStringEmpty(string &s){
+    for(auto i :s){
+        if(!isCharEmpty(i)){
+            return 0;
         }
     }
-    return true;
+    return 1;
 }
-void init(){
-    wordRephraser["long"]="long long";
-    wordRephraser["take"]="cin>>";
-    wordRephraser["say"]="cout<<";
-    wordRephraser["loop"]="for";
-}
-int main(int argCnt,char **args) {
-    vector<string> tabs(256);
-    tabs[0]="";
-    for(int i=1;i<tabs.size();i++){
-        tabs[i]=tabs[i-1];
-        tabs[i]+="\t";
-    }
-    init();
-    for(char c='a';c<='z';c++){
-        chartype[c]=1;
-    }    
-    for(char c='A';c<='Z';c++){
-        chartype[c]=1;
-    }    
-    for(char c='0';c<='9';c++){
-        chartype[c]=1;
-    }
-    chartype['_']=1;
-    char otherchars[]={'/', '\\', '"', '\'', ':', ';', '.', ',', '!', '+', '-', '*', '%',
-                         '<', '>', '(', ')', '{', '}', '[', ']', '|', '&', '^', '=', '?'};
-    for(char oc:otherchars){
-        chartype[oc]=20;
-    }
-
-    ifstream fin("example.txt");
-    ofstream fout("out.cpp");
-    fout<<"#include <bits/stdc++.h>\n\nusing namespace std;\n\n";
-    stringstream buffer;
-    buffer<<fin.rdbuf();
-    string code=buffer.str();
-
-    vector<string> words;
+vector<string> &tokenize(string code){
+    vector<string> &tokens=*(new vector<string>());
     string parser="";
-    int countMode=0;
+    int quotesMode=0;
+    /* quotesMode 0 for not under quotes
+    quotesMode 1 for under '
+    quoteMode 2 for under " */
+    int skipCharMode=0;
+    //will be used when '\' is used.
     for(char c:code){
-        if(chartype[c]==1 || countMode==1 || countMode==2){
+        if(skipCharMode){
             parser+=c;
-            if((countMode==1 && c=='\'') || (countMode==2 && c=='"')){
-                words.push_back(parser);
-                parser="";
-                countMode=0;
-            }
+            skipCharMode=0;
+            continue;
         }
-        else{
-            if(!isEmpty(parser)){
-                words.push_back(parser);
+        if(quotesMode && c=='\\'){
+            skipCharMode=1;
+            continue;
+        }
+        if(isCharEmpty(c) && !quotesMode){
+            if(!isStringEmpty(parser)){
+                tokens.push_back(parser);
             }
             parser="";
-            parser+=c;
-            if(c=='\'' || c=='"'){
-                if(c=='\''){
-                    countMode=1;
-                }
-                else{
-                    countMode=2;
-                }
+        }
+        else if(quotesMode){
+            if(quotesMode==1 && c=='\''){
+                parser+='\'';
+                tokens.push_back(parser);
+                parser="";
+                quotesMode=0;
+            }
+            else if(quotesMode==2 && c=='"'){
+                parser+='"';
+                tokens.push_back(parser);
+                parser="";
+                quotesMode=0;
             }
             else{
-                if(!isEmpty(parser)){
-                    words.push_back(parser);
-                }
-                parser="";
+                parser+=c;
             }
-            
-        }
-    }
-    if(!isEmpty(parser)){
-        words.push_back(parser);
-    }
-    string final_content="";
-    int indl=0;
-    int fw=1;
-    int writemode=0;
-    int loopargc=0;
-    string loopvar="";
-    int bc=0;
-    for(auto i:words){
-        if(fw && i!="}"){
-
-            final_content+=tabs[indl];
-            fw=0;
-        }
-        if(i=="long") i="long long";
-        if(i=="space") i="\" \"";
-        if(i=="nline") i="endl";
-        if(i!="}"){
-            if(writemode==0){
-                if(i=="take"){
-                    final_content+="cin>>";
-                    writemode=1;
-                }
-                else if(i=="say"){
-                    final_content+="cout<<";
-                    writemode=2;
-                }
-                else if(i=="loop"){
-                    final_content+="for";
-                    writemode=3;
-                    loopargc=-1;
-                    bc=0;
-                }
-                else{
-                    final_content+=i;
-                }
-            }
-            else if(writemode==1){
-                if(i==","){
-                    final_content+=">>";
-                }
-                else if(i==";"){
-                    final_content+=";";
-                    writemode=0;
-                }
-                else{
-                    final_content+=i;
-                }
-            }
-            else if(writemode==2){
-                if(i==","){
-                    final_content+="<<";
-                }
-                else if(i==";"){
-                    final_content+=";";
-                    writemode=0;
-                }
-                else{
-                    final_content+=i;
-                }
-            }
-            else if(writemode==3){
-                if(i=="("){
-                    bc+=1;
-                }
-                else if(i==")"){
-                    bc-=1;
-                    if(bc<0){
-                        throw runtime_error("() unmatched");
-                    }
-                }
-                if(loopargc==-1){
-                    final_content+=i;
-                    loopargc=0;
-                }
-                else if(i==","){
-                    loopargc=loopargc%2==0?loopargc+2:loopargc+1;
-                }
-                else if(loopargc==0){
-                    final_content+="long long ";
-                    final_content+=i;
-                    loopvar=i;
-                }
-                else if(loopargc==2){
-                    final_content+="= ";
-                    final_content+=i;
-                    loopargc=3;
-                }
-                else if(loopargc==3){
-                    final_content+=i;
-                }
-                else if(loopargc==4){
-                    final_content+="; ";
-                    final_content+=loopvar;
-                    final_content+=" < ";
-                    final_content+=i;
-                    loopargc=5;
-                }
-                else if(loopargc==5){
-                    if(bc==0){
-                        final_content+="; "+loopvar+"++ ";
-                        writemode=0;
-                    }
-                    final_content+=i;
-                }
-                
-            }
-        }
-        if(i==";"){
-            final_content+="\n";
-            fw=1;
-        }
-        else if(i=="{"){
-            final_content+="\n";
-            indl++;
-            fw=1;
-
-        }
-        else if(i=="}"){
-            indl--;
-            if(indl<0){
-                throw runtime_error("flour brackets not closed propoerly");
-            }
-            final_content+=tabs[indl];
-            final_content+="}\n";
-            fw=1;
-
         }
         else{
-            if(i!="=" && i!=";" && i!="+" && i!="-" && i!="*" && i!="/" && i!="%" && i!="(" && i!=")" && i!="{" && i!="}" && i!="[" && i!="]" && i!="^" && i!="&" && i!="|" && i!="!"){
-                final_content+=" ";
+            if(c=='\'' || c=='"'){
+                if(!isStringEmpty(parser)){
+                    tokens.push_back(parser);
+                }
+                parser=""; 
+                parser+=c;
+                quotesMode=c=='\''?1:2;
+            }
+            else if(CHAR_TYPE[c]==SpecialChar){
+                 if(!isStringEmpty(parser)){
+                    tokens.push_back(parser);
+                }
+                parser=""; 
+                parser+=c;
+                tokens.push_back(parser);
+                parser="";
+            }
+            else{
+                parser+=c;
             }
         }
+        
     }
-    fout<<final_content;
-    fin.close();
-    fout.close();
+    if(!isStringEmpty(parser)){
+        tokens.push_back(parser);
+    }
+    return tokens;
+}
+
+void initCharTypes(){
+    for(char c='a';c<='z';c++){
+        CHAR_TYPE[c]=TextOrNumber;
+    }
+    for(char c='A';c<='Z';c++){
+        CHAR_TYPE[c]=TextOrNumber;
+    }
+    for(char c='0';c<='9';c++){
+        CHAR_TYPE[c]=TextOrNumber;
+    }
+    //additionally '_' is assumed to be textOrNumber
+    CHAR_TYPE['_']=TextOrNumber;
+
+    char otherChars[]={'+','-','*','/','\\','%','!','<','>',',','.','\'',
+                        '"',';',':','[',']','{','}','(',')','^','&','|','?'};
+    for(char c:otherChars){
+        CHAR_TYPE[c]=SpecialChar;
+    }
+    CHAR_TYPE[' ']=SpaceOrEndline;
+    CHAR_TYPE['\n']=SpaceOrEndline;
+    CHAR_TYPE['\t']=SpaceOrEndline;
+    CHAR_TYPE['\r']=SpaceOrEndline;
+}
+void init(){
+    initCharTypes();
+
+}
+
+int main(int argCnt,char **args) {
+    init();
+    string examplecode="int  main(\"appke \nwo\nwo\\\' \"){}";
+    vector<string> tokens=tokenize(examplecode);
+    for(auto i : tokens){
+        cout<<i<<endl;
+    }
     return 0;
 }
